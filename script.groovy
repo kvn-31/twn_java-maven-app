000675654a5ccb9e6b81119b7576b9448d102202
kevinp <kevin.pirner@openforce.com>
2024-03-13 11:14:31 +0100

    fix: groovy script

--- script.groovy
+++ script.groovy
@@ -5,12 +5,13 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t kvnvna/demo-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+        sh 'docker push kvnvna/demo-app:jma-2.0'
     }
 }
+
 
 def deployApp() {
     echo 'deploying the application...'
